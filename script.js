4cc046e315ed09206caf03858334a3ff2ad0c0c3
rabeeharkd <secretroomuser@gmail.com>
2025-05-26 17:22:17 +0530

    Add all files

---
+++ script.js
@@ -0,0 +1,65 @@
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Common Sense illa lle , daa hamkee ,Press Add After Writing sth broohh")
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+
+    }
+
+    inputBox.value = "";
+    saveTask()
+}
+
+listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveTask();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveTask();
+    }
+})
+
+function saveTask() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+inputBox.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        addTask();
+    }
+
+})
+
+inputBox.addEventListener("keydown", (e) => {
+    if (e.key === "Shift" || e.key === "0") {
+        listContainer.innerHTML = "";
+    }
+})
+
+inputBox.addEventListener("keydown", (e) =>{ 
+    if (e.key === "-") {
+        let lastTask = listContainer.lastElementChild;
+        if (lastTask){
+            lastTask.remove();
+            saveTask();
+        }
+    }
+})
